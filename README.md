8fd044950e0b8d2b5148637a4867a16a7d5c638e
sylvain5410 <momie5410@gmail.com>
2026-07-19 22:39:05 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# ScaffR200 V3.4
+# ScaffR200 V3.5
 
-Version métier avec :
-- façades droites ou pignons ;
-- vue 3D avec trappes et échelles d’accès ;
-- arrêt des montants à la dernière lisse ;
-- vues de face, côté et dessus ;
-- travées visibles en vue de dessus ;
-- liste recalculée selon les niveaux variables du pignon.
-
-Déployer le contenu du dossier à la racine du dépôt GitHub Pages.
+Nouvelle interface métier responsive : tableau de bord 3D, vues face/côté/dessus, chevauchements visibles, réglages de pignon (hauteur basse, sous pente et pointe) et liste de matériel intégrée.
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='scaffr200-v34-cache-1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+const CACHE='scaffr200-v35-15';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
